c2be1189a12951e5edbaf4edebf80d87d5ff459a
ilya gorbunov <igorbunov@notanotherone.com>
2018-05-22 21:02:56 +0300

    fix scan script

--- atmo_scan_script.py
+++ atmo_scan_script.py
@@ -1,6 +1,7 @@
 import pexpect
 
 results = open("scan_results.txt", 'w')
+results.close()
 child = pexpect.spawn("bluetoothctl")
 child.logfile = open("/tmp/mylog", "wb")
 child.send("scan on\n")
@@ -8,8 +9,15 @@ bdaddrs = []
 
 try:
     while True:
-        child.expect("Device (([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})) ATMOTUBE")
-##        child.expect("Device (([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})).*")
+##        child.expect("Device (([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})) ATMOTUBE")
+        child.expect("Device (([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2}))", timeout=1000)
+        bdaddr = child.match.group(1)
+        results = open("scan_results.txt", 'a')
+        results.write(str(bdaddr))
+        results.close()
+        print(bdaddr)
+
+        
 ##        bdaddr = child.match.group(1)
 ##        param = child.after
 ##        if 'ATMOTUBE' in str(param):
@@ -17,9 +25,7 @@ try:
 ##            print(bdaddr)
         
 ##        print(param)
-        
-        bdaddr = child.match.group(1)
-        print(bdaddr)
+
 ##        if bdaddr not in bdaddrs:
 ##            bdaddrs.append(bdaddr)
 ##            results.write(str(bdaddr))
